e6de38c2d6f74930de89be95f437d3897a319094
piio-zicko <isaacijuo@gmail.com>
2022-08-31 16:03:34 +0000

    Gorilla NFT Updated Commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-require('dotenv').config()
+import "@nomiclabs/hardhat-ethers";
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_RINKEBY_API_KEY_URL = process.env.ALCHEMY_RINKEBY_API_KEY_URL;
+
+const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
     rinkeby: {
-      url: "RINKEBY_API_URL",
-      key: [PRIVATE_KEY],
+      url: ALCHEMY_RINKEBY_API_KEY_URL,
+      // @ts-ignore
+      accounts: [ACCOUNT_PRIVATE_KEY],
     },
 
 },
 }
 
-export default config;
